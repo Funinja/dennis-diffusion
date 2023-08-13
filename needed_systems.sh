ff720b44a4508a50272d4fba9def2a590a634a1a
Dennis Lam <denwt.lam@outlook.com>
2023-08-13 16:19:09 -0400

    updated setup file

--- needed_systems.sh
+++ needed_systems.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libopenmpi-dev
-sudo apt install ffmpeg
-sudo apt-get install python3-dev  # for python3.x installs
-sudo pip install --upgrade pip
+sudo apt-get install -y libopenmpi-dev
+sudo apt install -y ffmpeg
+sudo apt-get install -y python3-dev  # for python3.x installs
+sudo pip install --upgrade -y pip
