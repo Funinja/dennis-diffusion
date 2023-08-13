b8f4c176707eb8062d046daecd3d1db43efb30e5
Dennis Lam <denwt.lam@outlook.com>
2023-08-13 16:39:02 -0400

    fixed system setup

--- needed_systems.sh
+++ needed_systems.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y libopenmpi-dev
 sudo apt install -y ffmpeg
 sudo apt-get install -y python3-dev  # for python3.x installs
-sudo pip install --upgrade -y pip
+sudo pip install --upgrade pip
